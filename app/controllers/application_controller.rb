6e93c186fa9285dc1315bb5e2eee17bd4276d093
fadyHemaya <fady.hemaya@student.guc.edu.eg>
2022-04-09 20:50:24 +0200

    adding not found handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::API
     rescue_from ActionController::ParameterMissing, with: :render_unprocessable_entity
-  
+
     def render_unprocessable_entity(exception)
       render json: { error: exception.message }, status: :unprocessable_entity
     end
+    
+    def not_found
+      render json: {error:"Invalid Path"},status:  :not_found
+    end   
 end
